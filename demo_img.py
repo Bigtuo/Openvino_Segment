592ba361f30b2d8eefe27a071278492e881827aa
Bigtuo <67767982+Bigtuo@users.noreply.github.com>
2024-04-20 15:22:14 +0800

    Update demo_img.py

--- demo_img.py
+++ demo_img.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--model_name', type=str, default='STDCNet', choices=("STDCNet", "Deeplabv3"), help='分割算法选择')
     parser.add_argument('--model_onnx', type=str, default='weights/tdcnet.onnx', help='Path to ONNX model')
     parser.add_argument('--classes', type=str, default='Custom', choices=("Custom", "Cityscapes"), help='模型训练的类别')
-    parser.add_argument('--source', type=str, default=str('img/yw_2.jpg'), help='Path to input image')
+    parser.add_argument('--source', type=str, default=str('img/img.jpg'), help='Path to input image')
     parser.add_argument('--imgsz', type=tuple, default=(1024, 1024), help='Image input size')
     parser.add_argument('--infer_tool', type=str, default='openvino', choices=("openvino", "onnxruntime"), help='选择推理引擎')
     args = parser.parse_args()
